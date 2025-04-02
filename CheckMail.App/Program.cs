18f2cc50371d65a5506ab94262cbeb10e4320c22
George Focas <grfocas@gmail.com>
2025-04-03 00:58:16 +0100

    Add initial project setup with configs and dependencies
    This commit adds the initial setup for the CheckMail application, including project files, configurations, and dependency references. It includes `.gitignore` files, AssemblyInfo metadata, and binding redirects in App.config files for both the main project and test project. Necessary dependencies such as MailKit, MimeKit, and others are included in the project references.

---
+++ CheckMail.App/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace CheckMail.App
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var mailStuff = new MailStuff("localhost", 3025);
+            mailStuff.SendMail(
+                "user1@localhost",
+                "user2@localhost",
+                "Test Mail",
+                "This is a test mail"
+            );
+        }
+    }
+
+    public class MailStuff
+    {
+        private string _mailServer;
+        private int _mailPort;
+        
+        public MailStuff(string mailServer, int mailPort)
+        {
+            _mailServer = mailServer;
+            _mailPort = mailPort;
+        }
+
+        public void SendMail(string mailFrom, string mailTo, string mailSubject, string mailBody)
+        {
+            var messsage = new MimeMessage
+            {
+                Subject = mailSubject,
+                Body = new TextPart("plain") { Text = mailBody }
+            };
+            messsage.From.Add(new MailboxAddress("User 1", mailFrom));
+            messsage.To.Add(new MailboxAddress("User 2",  mailTo));
+            
+            using (var smtpClient = new SmtpClient())
+            {
+                smtpClient.Connect(_mailServer, _mailPort, false);
+                
+                
+                smtpClient.Send(messsage);
+                smtpClient.Disconnect(true);
+            }
+        }
+    }
+}
